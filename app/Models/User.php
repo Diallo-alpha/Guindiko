a5f07b6a589b2fcb2c0f6597eae0f65531312527
Oumyf <falladiaraoumy@gmail.com>
2024-08-10 21:56:40 +0000

    authentification avec jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable, HasRoles;
 
@@ -57,4 +58,19 @@ class User extends Authenticatable
       {
           return $this->hasOne(Mentee::class);
       }
+
+    //    Get the identifier that will be stored in the subject claim of the JWT.
+      
+     public function getJWTIdentifier()
+     {
+         return $this->getKey();
+     }
+ 
+     /**
+      * Return a key value array, containing any custom claims to be added to the JWT.
+      */
+     public function getJWTCustomClaims()
+     {
+         return [];
+     }
 }
