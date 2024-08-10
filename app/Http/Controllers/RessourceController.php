6d54fec505225ce9b05fc9d9e49117de1a1ad848
Aminata-A <aminataassane.ndiaye@gmail.com>
2024-08-10 22:48:22 +0000

    modification du crud ressource mise en place des request

---
+++ app/Http/Controllers/RessourceController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ressource;
+use App\Http\Requests\StoreRessourceRequest;
+use App\Http\Requests\UpdateRessourceRequest;
+use App\Models\Ressources;
+
+class RessourceController extends Controller
+{
+    // Lister toutes les ressources
+    public function index()
+    {
+        return Ressources::all();
+    }
+
+    // Afficher les détails d'une ressource spécifique
+    public function show($id)
+    {
+        return Ressources::findOrFail($id);
+    }
+
+    // Créer une nouvelle ressource
+    public function store(StoreRessourceRequest $request)
+    {
+        $ressource = Ressources::create($request->validated());
+        return response()->json($ressource, 201); // 201 Created
+    }
+
+    // Mettre à jour une ressource spécifique
+    public function update(UpdateRessourceRequest $request, $id)
+    {
+        $ressource = Ressources::findOrFail($id);
+        $ressource->update($request->validated());
+        return response()->json($ressource);
+    }
+
+    // Supprimer une ressource spécifique
+    public function destroy($id)
+    {
+        $ressource = Ressources::findOrFail($id);
+        $ressource->delete();
+        return response()->json(['message' => 'Ressource supprimée avec succès'], 204);
+    }
+}
--- app/Http/Requests/UpdateRessourcesRequest.php
+++ app/Http/Requests/UpdateRessourcesRequest.php
@@ -4,25 +4,19 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateRessourcesRequest extends FormRequest
+class UpdateRessourceRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'session_mentorat_id' => 'sometimes|exists:session_mentorats,id',
+            'titre' => 'sometimes|string|max:255',
+            'lien' => 'sometimes|url',
         ];
     }
 }
