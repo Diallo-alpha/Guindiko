bdb77bb834e543ab742b496ce04dc8dafb515e04
Cheikh <cheikhsane656@gmail.com>
2024-08-11 03:25:58 +0000

    ajoute de l'autentification de Oumy

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,22 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DomaineController;
+use App\Http\Controllers\FormationController;
+
+// Route protégée pour récupérer l'utilisateur authentifié
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Définir les routes pour le contrôleur Domaine
+Route::apiResource('domaines', DomaineController::class);
+
+// Définir les routes pour le contrôleur Formation
+Route::apiResource('formations', FormationController::class);
+
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
