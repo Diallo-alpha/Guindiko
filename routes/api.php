bcf151fae0b548f8f903ce4bca1893eaa7ae0e83
Diallo-alpha <alphaloppecity@gmail.com>
2024-08-11 01:56:17 +0000

    recupurer le crud de ressource et reservation et les functionalite authentification

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\RessourceController;
+
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+});
+    Route::apiResource('reservations', ReservationController::class);
+    Route::apiResource('ressources', RessourceController::class);
+
