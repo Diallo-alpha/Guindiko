dba3a79be31a1120f82a1db93d4f7ba04d1e09fe
Diallo-alpha <alphaloppecity@gmail.com>
2024-08-14 15:10:49 +0000

    matrialiser la relation entre forum et commentaire

--- app/Models/Forum.php
+++ app/Models/Forum.php
@@ -15,4 +15,8 @@ class Forum extends Model
     {
         return $this->belongsTo(Formation::class);
     }
+    public function commentaires()
+    {
+        return $this->hasMany(Commentaire::class);
+    }
 }
