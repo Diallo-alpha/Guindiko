71c6e602fa2b87426bde982885f44d397cd9db29
Diallo-alpha <alphaloppecity@gmail.com>
2024-08-25 01:15:09 +0000

    seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,56 +13,32 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-       
+        // Supprimer tous les utilisateurs existants
+        User::truncate();
+
+        // Supprimer les rôles existants si nécessaire
+        Role::truncate();
+
         // Créer les rôles
         $adminRole = Role::create(['name' => 'admin']);
         $mentorRole = Role::create(['name' => 'mentor']);
         $menteeRole = Role::create(['name' => 'mentee']);
 
-        // Créer les utilisateurs
-        $adminUser = User::create([
-            'name' => 'Admin',
-            'email' => 'adiaratououmyfall@gmail.com',
-            'password' => bcrypt('password'),
-            'parcours_academique' => 'Parcours académique par défaut',
-            'diplome' => 'Diplôme par défaut',
-            'langue' => 'Langue par défaut',
-            'cv' => null,
-            'experience' => 'Expérience par défaut',
-            'domaine' => 'Domaine par défaut',
-            'formation_id' => 1,
-            'role_id' => $adminRole->id,
-        ]);
-        $adminUser->assignRole($adminRole);
-
+        // Créer un nouvel utilisateur et lui attribuer le rôle de mentor
         $mentorUser = User::create([
-            'name' => 'Cheikh Sane',
-            'email' => 'cheikhsane656@gmail.com',
-            'password' => bcrypt('password'),
+            'name' => 'alpha',
+            'email' => 'alphaloppecity@gmail.com',
+            'password' => Hash::make('password'),
             'parcours_academique' => 'Master en Informatique',
             'diplome' => 'Master',
-            'langue' => 'Anglais',
-            'cv' => null,
-            'experience' => '3 ans en développement web et mobile',
-            'domaine' => 'Informatique',
-            'formation_id' => 1,
-            'role_id' => $mentorRole->id,
-        ]);
-        $mentorUser->assignRole($mentorRole);
-
-        $menteeUser = User::create([
-            'name' => 'Amina Ndiaye',
-            'email' => 'ndiayeamina775@gmail.com',
-            'password' => bcrypt('password'),
-            'parcours_academique' => 'Licence en Informatique',
-            'diplome' => 'Licence',
             'langue' => 'Français',
             'cv' => null,
-            'experience' => '2 ans en développement web',
+            'experience' => '5 ans en développement web',
             'domaine' => 'Informatique',
             'formation_id' => 1,
-            'role_id' => $menteeRole->id,
         ]);
-        $menteeUser->assignRole($menteeRole);
+
+        // Assigner le rôle de mentor
+        $mentorUser->assignRole($mentorRole);
     }
 }
